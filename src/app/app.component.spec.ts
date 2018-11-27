61caa6a1f8dbe3395f38759a89b87aebd1bfeb93
misscmunoz <miss.cmunoz@gmail.com>
2018-11-27 12:33:35 +0000

    task/2-tidy-item-styling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'FlickrApp'`, () => {
+  it(`should have as title 'FlickrFeed'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('FlickrFeed');
